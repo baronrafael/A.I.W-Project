127f5c9d25589e0bcefc0f16e26f804d426383cc
Unknown <baronrafael@hotmail.com>
2018-11-03 14:29:01 -0400

    Adding Project

---
+++ index.js
@@ -0,0 +1,17 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: "#000",
+  parent: "game-container",
+  pixelArt: true,
+  scene: [MainMenu, DungeonScene, MapEditor],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  }
+};
+
+var game = new Phaser.Game(config);
